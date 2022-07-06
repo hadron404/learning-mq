4ebe8776d2f2080369bcd14647dd8ea375110a6a
zhouqiang <2902511578@qq.com>
2022-07-06 11:25:52 +0800

    :sparkles: Start a quickstart project with SpringBoot + RabbitMQ + Gradle

---
+++ quickstart-rabbitmq/src/main/java/com/example/quickstartrabbitmq/QuickstartRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.example.quickstartrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstartRabbitmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstartRabbitmqApplication.class, args);
+	}
+
+}
---
+++ quickstart-rabbitmq/src/test/java/com/example/quickstartrabbitmq/QuickstartRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quickstartrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickstartRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

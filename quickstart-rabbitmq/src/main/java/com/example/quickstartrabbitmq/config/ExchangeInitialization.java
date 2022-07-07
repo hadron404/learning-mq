e477cb58e50c63901e5bff4a909b1d57e07fbf1d
zhouqiang <2902511578@qq.com>
2022-07-07 15:06:53 +0800

    :alembic: Use Java code to create exchange if not exist

---
+++ quickstart-rabbitmq/src/main/java/com/example/quickstartrabbitmq/config/ExchangeInitialization.java
@@ -0,0 +1,29 @@
+package com.example.quickstartrabbitmq.config;
+
+import com.example.quickstartrabbitmq.constants.ExchangeNames;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * RabbitMQ Exchange 初始化 .
+ *
+ * @author zhouqiang
+ * @since 2022/7/6
+ */
+@Configuration
+public class ExchangeInitialization {
+	/**
+	 * 默认的死信交换机
+	 * 配置消息交换机
+	 * 针对消费者配置
+	 * FanoutExchange： 将消息分发到所有的绑定队列，无routing-key的概念
+	 * HeadersExchange：通过添加属性key-value匹配
+	 * DirectExchange：按照routing-key分发到指定队列
+	 * TopicExchange：多关键字匹配
+	 */
+	@Bean
+	public DirectExchange defaultDLX() {
+		return new DirectExchange(ExchangeNames.DLX.getName(), true, false);
+	}
+}

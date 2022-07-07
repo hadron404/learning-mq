8b996f82fffdc083774e9489a4e6247e9236ea90
zhouqiang <2902511578@qq.com>
2022-07-07 15:34:39 +0800

    :alembic: Use Java code to binding queue and exchange if not exist

---
+++ quickstart-rabbitmq/src/main/java/com/example/quickstartrabbitmq/config/ExchangeQueueBinding.java
@@ -0,0 +1,26 @@
+package com.example.quickstartrabbitmq.config;
+
+import com.example.quickstartrabbitmq.constants.QueueNames;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 队列交换绑定关系初始化.
+ *
+ * @author zhouqiang
+ * @since 2022/7/7
+ */
+@Configuration
+public class ExchangeQueueBinding {
+
+	@Bean
+	Binding bindingTestDead(Queue testDead, DirectExchange defaultDLX) {
+		return BindingBuilder.bind(testDead)
+			.to(defaultDLX)
+			.with(QueueNames.TEST_DEAD);
+	}
+}
